bf9154d8d7fde9f9dc72730b11521309617fefb2
veNNNx <piotrek.cwalina1@wp.pl>
2023-07-07 20:12:43 +0200

    Change flask app path

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,20 +1,23 @@
-from backend.app import app
 import json
 
+from backend.api.app import app
+
+
 def test_index_route():
-    response = app.test_client().get('/')
+    response = app.test_client().get("/")
 
     assert response.status_code == 200
-    assert response.data.decode('utf-8') == 'Hello, World!'
+    assert response.data.decode("utf-8") == "Hello, World!"
+
 
 def test_get_data():
-    response = app.test_client().get('/data')
-    res = json.loads(response.data.decode('utf-8')).get("Data")
+    response = app.test_client().get("/data")
+    res = json.loads(response.data.decode("utf-8")).get("Data")
     assert type(res[0]) is dict
     assert type(res[1]) is dict
-    assert res[0]['id'] == 1
-    assert res[1]['id'] == 2
-    assert res[0]['dummy_value_1'] == 'Test'
-    assert res[1]['dummy_value_1'] == "Definitely not a test"
+    assert res[0]["id"] == 1
+    assert res[1]["id"] == 2
+    assert res[0]["dummy_value_1"] == "Test"
+    assert res[1]["dummy_value_1"] == "Definitely not a test"
     assert response.status_code == 200
     assert type(res) is list
